0b5e97bb2236f9fc5d686d9b764d5c0246124419
vernika302000 <67332514+vernika302000@users.noreply.github.com>
2023-09-28 23:35:36 +0530

    Create two_sum.java

---
+++ two_sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) 
+    {
+        int[] res=new int[2];
+        int i=0;
+        int j=nums.length-1;
+        while(i<j)
+        {
+            int sum=nums[i]nums[j];
+            if(sum==target)
+            {
+                res[0]=i;
+                res[1]=j;
+            }
+            else if(sum<target)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return res;
+    }
+}
